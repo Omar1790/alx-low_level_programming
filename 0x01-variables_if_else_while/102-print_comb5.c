34f40e2031b836c87100359d3a08b5513936044b
Omar <omar.t17900@gmail.com>
2023-08-14 19:46:23 -0700

    C

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdiio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0, y;
+
+	while (x <= 99)
+	{
+		x = y;
+		while (y <= 99)
+		{
+			if (y != x)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
