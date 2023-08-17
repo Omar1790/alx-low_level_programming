217073b258521fa958cb0001ca31132d054f3f42
Omar <omar.t17900@gmail.com>
2023-08-17 07:47:41 -0700

    C

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - function that draws a diagonal line on the terminal.
  *
- * @n: number of \ printed
+ * @n: number of special charcter printed
 */
 
 void print_diagonal(int n)
