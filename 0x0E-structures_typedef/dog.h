0108faad1c8db39f9e2720f7de39c9cfdee912f1
root <root@Omar>
2023-09-12 05:19:27 +0300

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-/***/
+/**
+ * struct dog - a dog struct
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: a dog struct containg the dog info
+*/
 
 struct dog
 {
